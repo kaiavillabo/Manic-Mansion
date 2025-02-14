eec59e5a9929b8b74680b211ccee67f4815411be
kaiavillabo <villabokaia@gmail.com>
2025-02-14 12:21:20 +0100

    rødhette henter sauer
    rødhette kan nå plukke opp sauer og ta dem med til den første frisonen. de dukker opp på samme "plass" i frisone1 som de var på i frisone2

--- rodhette.py
+++ rodhette.py
@@ -2,6 +2,7 @@ import pygame as pg
 from constants import *
 from bilder import *
 from character import Character
+from sau import Sau
 
 class Rodhette(Character):
     def __init__(self, x, y, spillebrett):
@@ -14,11 +15,10 @@ class Rodhette(Character):
         if not self.dead:
             keys_pressed = pg.key.get_pressed()
 
-            # Lagre de nye potensielle koordinatene
+            # nye potensielle koordinater
             new_x = self.x
             new_y = self.y
 
-            # Bevegelse basert på tastetrykk
             if keys_pressed[pg.K_LEFT]:
                 new_x -= RODHETTE1_SPEED
             if keys_pressed[pg.K_RIGHT]:
@@ -28,12 +28,22 @@ class Rodhette(Character):
             if keys_pressed[pg.K_UP]:
                 new_y -= RODHETTE1_SPEED
 
-            # Sjekk om hun går utenfor skjermen
+            # sjekker om hun går utenfor skjermen
             if 0 <= new_x <= WIDTH - self.width and 0 <= new_y <= HEIGHT - self.height:
                 # Sjekk om hun kolliderer med en busk
                 if not self.kolliderer_med_busker(new_x, new_y):
                     self.x = new_x
                     self.y = new_y
+
+            if not self.carry:  # kan kun plukke opp en sau om hun ikke allerede bærer en
+                for sau in self.spillebrett.sauer:
+                    if self.kolliderer_med_sau(sau, self.x, self.y):
+                        self.pick_up(sau)
+                        break  # stopper etter å ha plukket opp en sau
+
+            # sjekker om rødhette har nådd frisonen med en sau
+            if self.carry and self.x <= FREEZONE:
+                self.drop_sau()
 
             self.image = rodhette_image
 
@@ -50,3 +60,22 @@ class Rodhette(Character):
             new_y < busk.y + busk.height and
             new_y + self.height > busk.y
         )
+    
+    def kolliderer_med_sau(self, sau, new_x, new_y):
+        return (
+            new_x < sau.x + sau.width and
+            new_x + self.width > sau.x and
+            new_y < sau.y + sau.height and
+            new_y + self.height > sau.y
+        )
+
+    def pick_up(self, sau):
+        self.carry = True
+        self.carrying_sau = sau
+        sau.x = -100  # flytter sauen ut av skjermen
+
+    def drop_sau(self):
+        if self.carrying_sau:
+            self.carrying_sau.x = 25  # setter ny x-posisjon i frisonen, samme y
+            self.carry = False
+            self.carrying_sau = None
